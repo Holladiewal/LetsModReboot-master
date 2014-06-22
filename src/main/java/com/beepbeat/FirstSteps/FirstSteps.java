7e9d54635165e3e9ca456abbf8d9d75531f6c075
beepbeat <zfireman885@yahoo.de>
2014-06-22 17:29:32 +0200

    Mod Class Backbone added

---
+++ src/main/java/com/beepbeat/FirstSteps/FirstSteps.java
@@ -0,0 +1,31 @@
+package com.beepbeat.FirstSteps;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="FirstSteps", name="FirstSteps" ,version="1.7.2-1.0")
+public class FirstSteps
+{
+    @Mod.Instance("FirstSteps")
+    public static FirstSteps instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
